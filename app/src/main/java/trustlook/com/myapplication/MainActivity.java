7695f55c794c08fd272bea59b0e319d3ef287eca
Jinguo Wang <jinguo@trustlook.com>
2019-06-04 15:07:37 +0800

    test

--- app/src/main/java/trustlook/com/myapplication/MainActivity.java
+++ app/src/main/java/trustlook/com/myapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Reple widdddd33teee own 00ction", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Reple widdddd33teown 00ction", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
